2d00df91cc0e8a27fe0ef512a367a86bfd400dfa
keegannd <keegannd124@gmail.com>
2016-03-29 20:18:01 +0200

    finalcommit

--- ConsoleApplication3/deckofcards.cpp
+++ ConsoleApplication3/deckofcards.cpp
@@ -18,11 +18,65 @@ deckofcards::deckofcards()
 	}
 }
 
-deckofcards::deckofcards()
-{
+card deckofcards::draw() {
+	if (numcards != 0) {
+		card temp = *(deck[numcards - 1]);
+		delete deck[numcards - 1];
+		numcards -= 1;
+		return temp;
+	}
+	else
+		cout << "sorry there's no more cards" << endl;
+}
+int deckofcards::numberofcards() {
+	return numcards;
 }
 
+card deckofcards::peek() {
+	return *deck[numcards - 1];
+}
 
+deckofcards deckofcards::reset(){
+	return deckofcards();
+}
+
+void deckofcards::shuffle(){
+	if (numcards != 0) {
+		int i;
+		card* temp;
+		int random, random2;
+		for (i = 0; i < 50; i++) {
+			random = rand() % numcards;
+			random2 = rand() % numcards;
+			temp = deck[random];
+			deck[random] = deck[random2];
+			deck[random2] = temp;
+		}
+	}
+	else
+		cout << "Sorry there are no more cards in the Deck" << endl;
+}
+
+void deckofcards::print() {
+	int i;
+	for (i = 0; i < numcards; i++) {
+		cout << (*(deck[i])).number() << (*(deck[i])).colour() << endl;
+	}
+}
+
+D
 deckofcards::~deckofcards()
 {
 }
+
+int main(){
+	deckofcards c;
+	c.shuffle();
+	card Temp;
+	int i;
+	for (i = 0; i < 25; i++) {
+		Temp = c.draw();
+	}
+	return 0;
+}
+
